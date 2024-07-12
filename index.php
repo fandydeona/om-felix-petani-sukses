371579b8fd4c69e8b302839de7e0d7d1d86bfb84
fandydeona <fandydeona@gmail.com>
2024-07-12 09:27:01 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container p-2 my-2 bg-WHITE text-BLACK text-center">
+  <h1>OM FELIX, PEJUANG RUPIAH DEMI HIDUP KELUARGA KECIL-NYA </h1>
+  <p>"Jika kamu tidak berjuang untuk apa yang kamu inginkan, jangan menangis untuk apa yang tidak kamu dapatkan."</p>
+<p><img src="gambar/felix1.jpeg" width="100%" height="50%"></p> 
+<p> "udara pagi yang mengejutkan"............
+Om Felix, sapaan yang manja,merupakan salah satu karyawan klinik utama sint kamilus, bekerja sebagai seorang driver ambulance, melihat dengan keadaan ekonomi yang kurang, ia Kemudian berpikir, untuk meningkatkan kehidupan keluarga kecilnya, harus ada tindakan apa yang harus diambil??. Kemudian, dia berpikir untuk memanfatkan kesempatan.
+Ia ingin melakukan jasa tanam sayur, dengan menggunakan modal kerjanya sebagai driver ambulance. Jika kita ingin mengetetahui sosok om felix, kita bisa berjumpa dengan dia di klinik utama sint kamilus, beliau tiap hari berada di sana 
+. Dengan segelas kopi dan musik dari speaker bloetuth kecil, selalu memberi ia semangat dalam bekerja
+</p>
+</div>
+<div class="container p-1 my-1">
+  <div class="row">
+    <div class="col-sm-4">
+      <p><img src="gambar/felix2.jpeg" width="100%" height="50%"></p> 
+      <p>“Bertani tidak hanya bicara menanam, rawat dan panen. Tidak. Tetapi filosofi bertani dengan cara organik ini membuat kita bisa merenung bahwa kita ini lahir apakah sudah memberikan manfaat pada sekitar, atau justru memberikan kerusakan. Baik sisi kehidupan manusia dan lingkungan,” katanya.</p>
+      <p><img src="gambar/felix3.jpeg" width="100%" height="50%"></p> 
+    </div>
+    <div class="col-sm-8">
+      <p><img src="gambar/felix4.jpeg" width="100%" height="50%"></p> 
+      <p>Dorong kaum muda bertani
+
+Om felix, adalah satu sosok petani muda di negeri ini. Data memperlihatkan, petani muda di Indonesia, baru sekitar 10% dari total petani 33.4 juta. Prof. Andi Amran Sulaiman, Menteri Pertanian mengatakan, pertanian sangat terbuka untuk semua usia termasuk kaum muda.
+
+“Makin muda makin kuat, makin energik, makin kritis, makin apik kerjanya. Pertanian dengan semangat baru harus diluncurkan. Seperti membangun perilaku baru dan anak muda mendapatkan pendapatan jauh lebih baik dari pertanian,” katanya dalam keterangan tertulis di acara diskusi baru-baru ini.
+
+ </div>
+    
+  </div>
+  <p><img src="gambar/felix5.jpeg" width="100%" height="50%"></p> 
+</div>
+
+</body>
+</html>
